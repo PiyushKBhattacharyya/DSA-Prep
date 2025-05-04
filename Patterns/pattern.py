cc13193c7ac7d14c1cae046d2d300ea98ed91bd3
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-05-04 18:54:07 +0530

    patterns complete

--- Patterns/pattern.py
+++ Patterns/pattern.py
@@ -52,6 +52,49 @@ def pattern9(n):
 def pattern10(n):
     pattern2(n)
     pattern5(n-1)
+def pattern11(n):
+    for i in range(n):
+        start = 1 if i % 2 else 0
+        for j in range(i+1):
+            print(start, end="")  
+            start = 1 - start
+        print()
+def pattern12(n):
+    for i in range(n):
+        # Left numbers
+        for j in range(i + 1):
+            print(1 + j, end="")
+        # Spaces in between
+        spaces = 2 * (n - i - 1)
+        print(" " * spaces, end="")
+        # Right numbers (reversed)
+        for j in range(i, -1, -1):
+            print(1 + j, end="") 
+        print()
+def pattern13(n):    
+    num = 1
+    for i in range(n):
+        for j in range(i + 1):
+            print(num," ", end = "")
+            num += 1
+        print()
+def pattern14(n):
+    for i in range(n):
+        for j in range(ord('A'), ord('A') + i + 1):
+            print(chr(j), end="")
+        print()
+def pattern15(n):
+    if n % 2 == 0:
+        n += 1
+
+    for i in range(n):
+        for j in range(n):
+            if j == i or j == n - 1 - i:
+                print(' ', end='') 
+            else:
+                print('*', end='')
+        print()
+
 if __name__ == "__main__":
     n = int(input("Enter the size of the pattern (n): "))
-    pattern10(n)
+    pattern15(n)
