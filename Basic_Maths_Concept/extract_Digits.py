30b4486487aa4b827a5383fd88e28f62c761e33a
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-05-04 19:04:51 +0530

    basic_maths_concept-added

---
+++ Basic_Maths_Concept/extract_Digits.py
@@ -0,0 +1,12 @@
+def extract_digit(n):
+    digits = []
+    while n > 0:
+        ld = n % 10
+        digits.append(ld)
+        n = n // 10
+    
+    print(f"The digits of the number {n} are {digits[::-1]}")
+    
+if __name__ == "__main__":
+    n = int(input("Enter the digit: "))
+    extract_digit(n)
